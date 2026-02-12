269342635a862db82f647f12f904f4a112081df1
Ronja Rüther <ruether.ronja@fh-swf.de>
2026-02-13 00:03:20 +0100

    Logout aus Profil entfernt (ist jetzt unter Einstellungen)

--- src/views/profil/Profil.vue
+++ src/views/profil/Profil.vue
@@ -1,21 +1,3 @@
-<script setup lang="ts">
-import { getAuth, signOut } from 'firebase/auth'
-import { useAuthStore } from '@/stores/authStore'
-import router from '@/router'
-
-const authStore = useAuthStore()
-const auth = getAuth()
-
-const logout = async () => {
-  await signOut(auth)
-  authStore.$patch({ aktuellesKonto: null })
-  router.push({ name: 'login' })
-}
-</script>
-
 <template>
   <h1>Profil</h1>
-  <div>
-    <Button @click="logout">Logout</Button>
-  </div>
 </template>
